d785a079488cfe874b33dc0064bd7ef700e4d0da
magur0don <magurodon.10000yen@gmail.com>
2020-08-25 21:23:15 +0900

    aa

--- 3DActionGame/Assets/Script/CharacterMoveController.cs
+++ 3DActionGame/Assets/Script/CharacterMoveController.cs
@@ -7,9 +7,17 @@ public class CharacterMoveController : MonoBehaviour
     private Animator m_animator = null;
     private Rigidbody m_rigidbody = null;
     private Vector3 m_direction = Vector3.zero;
+
     private CharacterController m_characterController = null;
     private float m_turnSmoothVerocity = 0f;
     private float m_turnSmoothTime = 0.1f;
+
+    private float m_gravityValue = -9.81f;
+    private Vector3 m_playerGravityVelocity = Vector3.zero;
+
+    private bool m_jumpFlag = false;
+
+    private bool m_punchFlag = false;
 
     private void Start()
     {
@@ -22,21 +30,54 @@ public class CharacterMoveController : MonoBehaviour
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+
+        m_jumpFlag = Input.GetKeyDown(KeyCode.Space);
+
+        m_punchFlag = Input.GetKeyDown(KeyCode.Z);
+
         Move(h, v);
     }
 
     private void Move(float horizontal, float vertical)
     {
-        m_direction = new Vector3(horizontal, 0, vertical).normalized;
-
-        if (m_direction.magnitude >= 0.1f)
+        if (m_punchFlag)
         {
-            var targetAngle = Mathf.Atan2(m_direction.x, m_direction.z) * Mathf.Rad2Deg;
+            m_animator.SetTrigger("Punch");
+        }
+
+        if (m_jumpFlag && m_characterController.isGrounded)
+        {
+            m_playerGravityVelocity.y = 6;
+            m_animator.SetTrigger("Jump");
+            m_characterController.Move(m_playerGravityVelocity * Time.deltaTime);
+            m_jumpFlag = false;
+        }
+
+        var cameraFoward = Camera.main.transform.forward;
+        cameraFoward.y = 0;
+        cameraFoward = cameraFoward.normalized;
+        if (cameraFoward.sqrMagnitude < 0.01f)
+            return;
+
+        Quaternion inputFrame = Quaternion.LookRotation(cameraFoward, Vector3.up);
+        var input = new Vector3(horizontal, 0, vertical);
+        var cameraFromPlayer = inputFrame * input;
+
+        if (cameraFromPlayer.sqrMagnitude >= 0.1f)
+        {
+            var targetAngle = Mathf.Atan2(cameraFromPlayer.x, cameraFromPlayer.z) * Mathf.Rad2Deg;
             var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref m_turnSmoothVerocity, m_turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-            m_characterController.Move(m_direction * Speed * Time.deltaTime);
+            m_characterController.Move(cameraFromPlayer * Speed * Time.deltaTime);
         }
-        m_animator.SetFloat("FrontVelocity", m_direction.magnitude);
+        
+        if (!m_jumpFlag && !m_characterController.isGrounded)
+        {
+            m_playerGravityVelocity.y += m_gravityValue * Time.deltaTime;
+            m_characterController.Move(m_playerGravityVelocity * Time.deltaTime);
+        }
+
+        m_animator.SetFloat("FrontVelocity", cameraFromPlayer.magnitude);
     }
 
     public void FootR()
