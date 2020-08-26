528017a5185f7ee2c8d0fe22b5bc3532d9ce54bf
magur0don <magurodon.10000yen@gmail.com>
2020-08-26 22:50:25 +0900

    moveZombie

---
+++ 3DActionGame/Assets/Script/ZombieMoveController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieMoveController : MonoBehaviour
+{
+
+    private NavMeshAgent m_navMeshAgent = null;
+
+    [SerializeField]
+    private Transform m_targetTransform = null;
+
+    private Animator m_animator = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_navMeshAgent = GetComponent<NavMeshAgent>();
+
+        m_animator = GetComponent<Animator>();
+
+    }
+
+    private void Update()
+    {
+        if (m_targetTransform != null)
+        {
+            m_navMeshAgent.SetDestination(m_targetTransform.position);
+          
+            if (m_navMeshAgent.remainingDistance > 0.1f)
+            {
+                m_animator.SetFloat("FrontVelocity", m_navMeshAgent.velocity.sqrMagnitude);
+            }
+        }
+    }
+
+}
